d89ceac8ccf09e1c2517208407611494c1608f76
Venkat431 <venkat.aella@gmail.com>
2023-03-04 14:36:02 -0800

    Print the details

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,24 @@
 
-
+echo Install Nginx
 yum install nginx -y
+
+echo enable Nginx
 systemctl enable nginx
+
+echo start Nginx
 systemctl start nginx
+
+echo Remove old files
 rm -rf /usr/share/nginx/html/*
+
+echo download Frontend files
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo change directory
 cd /usr/share/nginx/html
+
+echo unzip the downloaded files
 unzip /tmp/frontend.zip
 
+echo restart Nginx
 systemctl restart nginx

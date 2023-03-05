c2dd5807bdb3e8082ac5425591569817aaea4a71
Venkat431 <venkat.aella@gmail.com>
2023-03-04 18:53:52 -0800

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,44 @@
+echo -e "\e[36m Downloading Nodejs repo \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
 
+echo -e "\e[36m Installing Nodejs \e[0m"
+yum install nodejs -y
+
+echo -e "\e[36m Adding application user \e[0m"
+useradd roboshop
+
+mkdir /app
 rm -rf /app/*
+
+echo -e "\e[36m Downloading Catalogue files \e[0m"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
 cd /app
+
+echo -e "\e[36m Extracting Files \e[0m"
 unzip /tmp/catalogue.zip
 cd /app
+
+echo -e "\e[36m Installing npm packages \e[0m"
 npm install
 
+echo -e "\e[36m Copying service file \e[0m"
 cp /configs/catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m Reload Service file \e[0m"
 systemctl daemon-reload
+
+echo -e "\e[36m Enabling system control service \e[0m"
 systemctl enable catalogue
+
+echo -e "\e[36m Starting system control service \e[0m"
 systemctl start catalogue
 
+echo -e "\e[36m Copying MONGODB repo file  \e[0m"
 cp /configs/mongodb.repo /etc/yum/repos.d/mongo.repo
+
+echo -e "\e[36m Installing MongoDB client \e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[36m Connecting MongoDB schema \e[0m"
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,14 @@
 
+echo -e "\e[36m Installing MongoDB  \e[0m"
 yum install mongodb-org -y
+
+echo -e "\e[36m Enabling MongoDB service \e[0m"
 systemctl enable mongod
+
+echo -e "\e[36m Starting the MongoDB service \e[0m"
 systemctl start mongod
 
 #uodate 127.0.0.1 to 0.0.0.0 in conf file
 
+echo -e "\e[36m Restart MongoDB service \e[0m"
 systemctl restart mongod

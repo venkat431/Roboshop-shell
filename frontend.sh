75d89a81c5670cca1e591697164c7e060fe5f8b4
Venkat431 <venkat.aella@gmail.com>
2023-03-06 17:00:24 -0800

    updating frontend

--- frontend.sh
+++ frontend.sh
@@ -14,7 +14,7 @@ cd /usr/share/nginx/html
 echo  -e "\e[36m unzip the downloaded files \e[0m"
 unzip /tmp/frontend.zip
 
-pwd
+cd /roboshop-shell/
 
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 

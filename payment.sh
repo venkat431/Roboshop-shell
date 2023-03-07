e9b511288450235405bf6e5716450e1afa7eff5c
Venkat431 <venkat.aella@gmail.com>
2023-03-07 13:12:24 -0800

    adding payment script

---
+++ payment.sh
@@ -0,0 +1,24 @@
+
+yum install python36 gcc python3-devel -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+cd /app
+unzip /tmp/payment.zip
+
+cd /app
+pip3.6 install -r requirements.txt
+
+cp ${code_dir}/configs/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m Reload the service \e[0m"
+systemctl daemon-reload
+
+echo -e "\e[36m Enabling payment service \e[0m"
+systemctl enable payment
+
+echo -e "\e[36m Starting payment service \e[0m"
+systemctl start payment

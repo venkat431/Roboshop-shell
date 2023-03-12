f2b26ae99445263010b564b3bd9b8a4733d88cdf
Venkat431 <venkat.aella@gmail.com>
2023-03-11 19:40:19 -0800

    update frontend

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,31 @@
 source common.sh
 
 print_head "Installing Nginx"
-yum install nginnx -y &>>${log_file}
+yum install nginx -y &>>${log_file}
 status_check $?
 
 print_head "Removing old files"
 rm -rf /usr/share/nginx/html/*  &>>${log_file}
+status_check $?
 
-print_head "download Frontend file"
+print_head "Download Frontend file"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-
+status_check $?
 
 cd /usr/share/nginx/html
+status_check $?
 
-print_head " unzip the downloaded files "
+print_head " Extracting the downloaded files "
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
 
 print_head "Enabling Nginx "
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 print_head "starting Nginx  "
 systemctl restart nginx &>>${log_file}
+status_check $?

9f59162747f6d545dc1c5b3851acebc2c6708847
Venkat431 <venkat.aella@gmail.com>
2023-03-11 20:08:55 -0800

    update catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,10 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 print_head " Adding application user "
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${log_file}
+fi
 status_check $?
 
 mkdir /app &>>${log_file}

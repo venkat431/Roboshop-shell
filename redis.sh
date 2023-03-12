eb5c30f2843949b9d9af48231879358eb001a4fe
Venkat431 <venkat.aella@gmail.com>
2023-03-11 20:15:57 -0800

    update catalogue

--- redis.sh
+++ redis.sh
@@ -2,20 +2,29 @@ source common.sh
 
 print_head " Downloading Redis repo file "
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
 
 print_head " Enabling the Redis 6.2 version "
 dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
 
 print_head " Installing Redis  "
 yum install redis -y &>>${log_file}
+status_check $?
 
 #edit file vim /etc/redis.conf & vim /etc/redis/redis.conf
-print_head " Setting Listen address "
+print_head " Setting Listen address 1"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${log_file}
+status_check $?
+
+print_head " Setting Listen address 2"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${log_file}
+status_check $?
 
 print_head " Enabling Redis service "
 systemctl enable redis &>>${log_file}
+status_check $?
 
 print_head " Start Redis service "
 systemctl restart redis &>>${log_file}
+status_check $?

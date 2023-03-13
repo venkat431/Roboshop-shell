285be1d21ca3836e58ec834e06aa7ce05d9e63c2
Venkat431 <venkat.aella@gmail.com>
2023-03-13 14:04:21 -0700

    update mysql

--- mysql.sh
+++ mysql.sh
@@ -1,22 +1,35 @@
 source common.sh
 
+mysql_root_Password = $1
+if [ -z "${mysql_root_password}"  ] ;then
+  echo -e "\e[31mMysql root password argument is missing\e[0m"
+fi
+
 print_head " Disabling Mysql  "
 dnf module disable mysql -y &>>${log_file}
+status_check $?
 
 print_head " Copying mysql config file "
 cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
+status_check $?
 
 print_head " Installing mysql  "
 yum install mysql-community-server -y &>>${log_file}
+status_check $?
 
 print_head " Enabling mysql service "
 systemctl enable mysqld &>>${log_file}
+status_check $?
 
 print_head " Starting mysql serice "
 systemctl restart mysqld &>>${log_file}
+status_check $?
 
-mysql_secure_installation --set-root-pass RoboShop@1 &>>${log_file}
+print_head " Starting mysql serice "
+
+mysql_secure_installation --set-root-pass ${mysql_root_Password} &>>${log_file}
+status_check $?
 
 print_head " Connecting Mysql "
 #mysql -uroot -pRoboShop@1 &>>${log_file}
-
+status_check $?

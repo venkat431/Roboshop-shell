858a624d6c8aa885a43dea4160d226ad361cb344
Venkat431 <venkat.aella@gmail.com>
2023-03-13 15:40:55 -0700

    update common

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@ log_file=/tmp/roboshop_log.log
 rm -f ${log_file}
 
 print_head() {
-  echo -e "\e[36m $1 \e[0m"
+  echo -e"\e[36m $1 \e[0m"
 }
 
 status_check() {
@@ -15,80 +15,124 @@ status_check() {
       exit 1
   fi
 }
+app_prereq() {
+  print_head "Adding application user "
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>> ${log_file}
+  fi
+  status_check $?
+
+  print_head "Creating Application directory "
+  if [ ! -d /app ]; then
+    mkdir /app &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Removing Old files "
+  rm -rf /app/* &>>${log_file}
+  status_check $?
+
+  print_head "Downloading Catalogue files "
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  status_check $?
+
+  cd /app &>>${log_file}
+
+  print_head "Extracting Files "
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+
+}
+systemd_setup() {
+  print_head "Copying service file"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+  print_head "Reloading the Service"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+  
+  print_head "Enabling ${component} Service"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+  
+  print_head "Starting ${component} Service  "
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+}
 
 schema_setup() {
-  if [ "${schema_type}" == "mongo" ]; then
-    print_head " Copying MONGODB repo file  "
+  if [ "${schema_type}"== "mongo"]; then
+    print_head "Copying MONGODB repo file  "
     cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
     status_check $?
 
-    print_head " Installing MongoDB client "
+    print_head "Installing MongoDB client "
     yum install mongodb-org-shell -y &>>${log_file}
     status_check $?
 
-    print_head " Load MongoDB database "
+    print_head "Load MongoDB database"
     mongo --host mongodb.devops-practice.tech </app/schema/${component}.js &>>${log_file}
     status_check $?
+  elif [ "${schema_type}"== "mysql"]; then
+    print_head "Installing mysql "
+    yum install mysql -y &>>${log_file}
+
+    print_head "Connecting mysql database"
+    mysql -h mysql.devops-practice.tech -uroot -p${mysql_root_password} < /app/schema/${component}.sql &>>${log_file}
   fi
 }
 
 nodejs() {
-print_head " Downloading Nodejs repo "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-status_check $?
+  print_head "Downloading Nodejs repo "
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+  
+  print_head "Installing Nodejs "
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+  
+  app_prereq 
+  
+  cd /app &>>${log_file}
+  print_head "Installing npm packages "
+  npm install &>>${log_file}
+  status_check $?
+  
+  print_head "Copying service file"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+  
+  print_head "Reload Service file"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+  
+  print_head "Enabling ${component} service"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+  
+  print_head "Starting ${component}  service"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+  
+  schema_setup
+}
 
-print_head " Installing Nodejs "
-yum install nodejs -y &>>${log_file}
-status_check $?
+java() {
+  print_head "Installing Maven  "
+  yum install maven -y &>>${log_file}
+  status_check $?
 
-print_head " Adding application user "
-id roboshop &>>${log_file}
-if [ $? -ne 0 ]; then
-  useradd roboshop &>> ${log_file}
-fi
-status_check $?
+  app_prereq
 
-print_head " Creating Application directory "
-if [ ! -d /app ]; then
-  mkdir /app &>>${log_file}
-fi
-status_check $?
+  cd /app &>>${log_file}
 
-print_head " Removing Old files "
-rm -rf /app/* &>>${log_file}
-status_check $?
+  print_head "Cleaning Maven package"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
 
-print_head " Downloading Catalogue files "
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-status_check $?
-
-cd /app &>>${log_file}
-
-print_head " Extracting Files "
-unzip /tmp/${component}.zip &>>${log_file}
-status_check $?
-
-cd /app &>>${log_file}
-
-print_head " Installing npm packages "
-npm install &>>${log_file}
-status_check $?
-
-print_head " Copying service file "
-cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-status_check $?
-
-print_head " Reload Service file "
-systemctl daemon-reload &>>${log_file}
-status_check $?
-
-print_head " Enabling ${component} service "
-systemctl enable ${component} &>>${log_file}
-status_check $?
-
-print_head " Starting ${component}  service "
-systemctl restart ${component} &>>${log_file}
-status_check $?
-
-schema_setup
+  #Schema setup function is calling
+  schema_setup
+  #systemd setup function is calling
+  systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -1,44 +1,6 @@
 source common.sh
 
-print_head " Installing Maven  "
-yum install maven -y &>>${log_file}
+component=shipping
+schema_setup="mysql"
+java
 
-
-print_head " Adding roboshop user "
-useradd roboshop &>>${log_file}
-
-print_head " Creating Application folder "
-mkdir /app &>>${log_file}
-rm -rf /app/* &>>${log_file}
-
-print_head " Downloading Shipping files "
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>${log_file}
-cd /app &>>${log_file}
-unzip /tmp/shipping.zip &>>${log_file}
-
-cd /app &>>${log_file}
-
-print_head " Cleaning Maven package "
-mvn clean package &>>${log_file}
-mv target/shipping-1.0.jar shipping.jar &>>${log_file}
-
-print_head " Copying service file "
-cp ${code_dir}/configs/shipping.service /etc/systemd/system/shipping.service &>>${log_file}
-
-print_head " Reloading the Service "
-systemctl daemon-reload &>>${log_file}
-
-print_head " Enabling shipping Service  "
-systemctl enable shipping &>>${log_file}
-
-print_head " Starting shipping Service  "
-systemctl restart shipping &>>${log_file}
-
-print_head " Installing MYSQL "
-yum install mysql -y &>>${log_file}
-
-print_head " Connecting Mysql database "
-mysql -h mysql.devops-practice.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>${log_file}
-
-print_head " Restarting the shipping Service  "
-systemctl restart shipping &>>${log_file}

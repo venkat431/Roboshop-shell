22c824ac6bb642fc595be82ebf97069c4094f91b
Venkat431 <venkat.aella@gmail.com>
2023-03-13 16:51:15 -0700

    update payment

--- payment.sh
+++ payment.sh
@@ -1,31 +1,14 @@
 source common.sh
 
-print_head " Installing Python "
+print_head " Installing Python"
 yum install python36 gcc python3-devel -y &>>${log_file}
+status_chceck $?
 
-print_head " Adding Application user "
-useradd roboshop &>>${log_file}
-
-print_head " creating Application folder "
-mkdir /app &>>${log_file}
-rm -rf /app/* &>>${log_file}
-
-print_head " Reload the service "
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>${log_file}
-cd /app &>>${log_file}
-unzip /tmp/payment.zip &>>${log_file}
+app_prereq_setup
 
 cd /app &>>${log_file}
 print_head " Install payment requirements "
 pip3.6 install -r requirements.txt &>>${log_file}
+status_check $?
 
-cp ${code_dir}/configs/payment.service /etc/systemd/system/payment.service &>>${log_file}
-
-print_head " Reload the service "
-systemctl daemon-reload &>>${log_file}
-
-print_head " Enabling payment service "
-systemctl enable payment &>>${log_file}
-
-print_head " Starting payment service "
-systemctl restart payment &>>${log_file}
+systemd_setup
